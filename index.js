4245af4e4b6d3cebc0ca146671e008775a65f88f
Nivaldo Bondanca <hick209@gmail.com>
2017-03-19 20:23:08 -0300

    Added things for production readiness

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('output/prod/'));
+
+const listener = app.listen(process.env.PORT || 80, () => {
+    console.info(`Server started on port ${listener.address().port}!`);
+});
