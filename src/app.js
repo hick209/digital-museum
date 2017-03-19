a3953b1942db9c07503dd4c16fd4180b1ce23637
Nivaldo Bondanca <hick209@gmail.com>
2017-03-19 18:25:44 -0300

    Added Material UI

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/App'
 import { constants } from './strings'
 import storeFactory from './store'
 import sampleData from './store/initialState'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const storageData = localStorage[constants.LOCAL_STORAGE_KEY]
 const initialState = (storageData) ? JSON.parse(storageData) : sampleData
@@ -13,9 +18,6 @@ const saveState = () =>
     localStorage[constants.LOCAL_STORAGE_KEY] = JSON.stringify(store.getState())
 
 const store = storeFactory(initialState)
-
-window.store = store
-window.React = React
 
 render(
   <Provider store={store}>
--- src/components/ui/App.js
+++ src/components/ui/App.js
@@ -1,8 +1,32 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { blueGrey500, blueGrey700, lightGreenA200 } from 'material-ui/styles/colors'
+import AppBar from 'material-ui/AppBar'
+import RaisedButton from 'material-ui/RaisedButton'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blueGrey500,
+    primary2Color: blueGrey700,
+    // primary3Color: grey400,
+    accent1Color: lightGreenA200,
+    // accent2Color: grey100,
+    // accent3Color: grey500,
+  },
+});
+
 const App = () => (
-  <div>
-    <h1>This is the app</h1>
-    <p>With a simple message</p>
-  </div>
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div>
+      <AppBar title="Title"/>
+      <h1>This is the app</h1>
+      <p>With a simple message</p>
+      <RaisedButton label="Default" primary={true}/>
+      <FloatingActionButton/>
+    </div>
+  </MuiThemeProvider>
 )
 
 export default App
