34d417d4b68ebee29061acc828ff5d59292cb5e5
Nivaldo Bondanca <hick209@gmail.com>
2017-04-08 17:17:16 -0300

    Configured Firebase access

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,12 @@
 import firebase from 'firebase'
 
-console.info(firebase)
+const devConfigs = {
+  apiKey: 'AIzaSyCRSy5EK3ihLsWict8nV2Ykyr3aMb1au9g',
+  authDomain: 'digital-museum-5d606.firebaseapp.com',
+  databaseURL: 'https://digital-museum-5d606.firebaseio.com',
+  projectId: 'digital-museum-5d606',
+  storageBucket: 'digital-museum-5d606.appspot.com',
+  messagingSenderId: '569794739485',
+}
+
+firebase.initializeApp(devConfigs)
