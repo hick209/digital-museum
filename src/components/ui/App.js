a2f4c86b4257a5b6342809b0ba0e7816baa355a7
Nivaldo Bondanca <hick209@gmail.com>
2017-04-10 09:53:41 -0300

    Added loading to main screen

--- src/components/ui/App.js
+++ src/components/ui/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CircularProgress from 'material-ui/CircularProgress'
 import { BrowserRouter as Router, Route, Redirect, Switch, IndexRoute } from 'react-router-dom'
 import Theme from './Theme'
 import Toolbar from './Toolbar'
@@ -6,16 +7,24 @@ import Collections from '../Collections'
 import CollectionItems from '../CollectionItems'
 import Whoops404 from '../Whoops404'
 
-const App = ({ title, children }) => (
+const App = ({ title, pageLoading, children }) => (
   <Router>
     <Theme>
       <div>
         <Toolbar title={ title }/>
-        <Switch>
-          <Route exact path='/' component={ Collections }/>
-          <Route path='/collections/:collectionId' component={ CollectionItems }/>
-          <Redirect to='/'/>
-        </Switch>
+        {
+          pageLoading ? (
+            <div className='centerGravity'>
+              <CircularProgress style={{ margin: 32 }}/>
+            </div>
+          ) : (
+            <Switch>
+              <Route exact path='/' component={ Collections }/>
+              <Route path='/collections/:collectionId' component={ CollectionItems }/>
+              <Redirect to='/'/>
+            </Switch>
+          )
+        }
       </div>
     </Theme>
   </Router>
