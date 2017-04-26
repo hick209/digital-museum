e401b770d4a9c1d13e962bd4da1a610fbd07a45a
Nivaldo Bondanca <hick209@gmail.com>
2017-04-26 18:36:33 -0300

    Refactored buildScripts

--- buildScripts/base.js
+++ buildScripts/base.js
@@ -3,66 +3,66 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = (path, outputPath) => ({
-  entry: {
-    app: './src/app.js',
-  },
-  output: {
-    path: outputPath,
-    filename: path.join('js', '[name].[chunkhash].js'),
-    // publicPath: '/',
-    sourceMapFilename: path.join('js', '[name].[chunkhash].map'),
-  },
-  devtool: 'source-map',
-  // resolve: {
-  //   extensions: ['.js', '.json'],
-  //   modules: [ path.join(__dirname, 'src'), 'node_modules' ]
-  // },
-  module: {
-    loaders: [
-      {
-        test: /\.hbs$/,
-        loader: 'handlebars-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['latest', 'react'],
-        },
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader'
-      },
-      {
-        test: /\.scss/,
-        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.hbs',
-      chunksSortMode: 'dependency',
-    }),
-    new CopyWebpackPlugin([
-      // Copy app icons
-      { from: './res/icons', to: 'icons' },
-      // Application Manifest
-      { from: './src/manifest.json' },
-      // Dynamic block for Microsoft
-      { from: './src/browserconfig.xml' },
-    ]),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'commons',
-      filename: path.join('js', 'commons.[chunkhash].js'),
-    }),
+	entry: {
+		app: './src/app.js',
+	},
+	output: {
+		path: outputPath,
+		filename: path.join('js', '[name].[chunkhash].js'),
+		// publicPath: '/',
+		sourceMapFilename: path.join('js', '[name].[chunkhash].map'),
+	},
+	devtool: 'source-map',
+	// resolve: {
+	//   extensions: ['.js', '.json'],
+	//   modules: [ path.join(__dirname, 'src'), 'node_modules' ]
+	// },
+	module: {
+		loaders: [
+			{
+				test: /\.hbs$/,
+				loader: 'handlebars-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['latest', 'react'],
+				},
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.hbs',
+			chunksSortMode: 'dependency',
+		}),
+		new CopyWebpackPlugin([
+			// Copy app icons
+			{ from: './res/icons', to: 'icons' },
+			// Application Manifest
+			{ from: './src/manifest.json' },
+			// Dynamic block for Microsoft
+			{ from: './src/browserconfig.xml' },
+		]),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'commons',
+			filename: path.join('js', 'commons.[chunkhash].js'),
+		}),
 
-    // new ForkCheckerPlugin(),
-  ]
+		// new ForkCheckerPlugin(),
+	]
 })
--- buildScripts/prod.js
+++ buildScripts/prod.js
@@ -4,36 +4,36 @@ const OfflinePlugin = require('offline-plugin')
 const commonConfig = require('./base.js')
 
 module.exports = (env, path, outputPath) => webpackMerge(commonConfig(path, outputPath), {
-  plugins: [
-    // new webpack.LoaderOptionsPlugin({
-    //     minimize: true,
-    //     debug: false
-    // }),
+	plugins: [
+		// new webpack.LoaderOptionsPlugin({
+		//     minimize: true,
+		//     debug: false
+		// }),
 
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
 
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true,
-        warnings: false
-      },
-      comments: false,
-      sourceMap: true,
-    }),
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			beautify: false,
+			mangle: {
+				screw_ie8: true,
+				keep_fnames: true
+			},
+			compress: {
+				screw_ie8: true,
+				warnings: false
+			},
+			comments: false,
+			sourceMap: true,
+		}),
 
-    // According to the documentation, it's always better if OfflinePlugin is the last plugin added
-    new OfflinePlugin({
-      AppCache: false
-    }),
-  ],
+		// According to the documentation, it's always better if OfflinePlugin is the last plugin added
+		new OfflinePlugin({
+			AppCache: false
+		}),
+	],
 })
