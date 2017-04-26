7bec7936a1864104a61e600257235fdd3c3ddf73
Nivaldo Bondanca <hick209@gmail.com>
2017-04-26 15:22:09 -0300

    Created sample state

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import storeFactory from './store'
 import initialData from './store/initialState'
 import './stylesheets/app.scss'
 
-import { setMuseumId, fetchMuseum, addError } from './actions'
+import { setMuseumId, addError } from './actions'
 
 uiSetup()
 
@@ -20,42 +20,43 @@ setupErrorHandler(store)
 offlineSetup()
 
 render(
-  <Provider store={ store }>
-    <App/>
-  </Provider>,
-  document.getElementById('app')
+		<Provider store={ store }>
+			<App/>
+		</Provider>,
+		document.getElementById('app')
 )
 
 
 function setupDatastore() {
-  const storageData = localStorage[ constants.LOCAL_STORAGE_KEY ]
-  const initialState = (storageData) ? JSON.parse(storageData) : initialData
+	const storageData = localStorage[ constants.LOCAL_STORAGE_KEY ]
+	const initialState = (storageData) ? JSON.parse(storageData) : initialData
 
-  const saveState = () =>
-      localStorage[constants.LOCAL_STORAGE_KEY] = JSON.stringify(store.getState())
+	const saveState = () =>
+			localStorage[ constants.LOCAL_STORAGE_KEY ] = JSON.stringify(store.getState())
 
-  const store = storeFactory(initialState)
+	const store = storeFactory(initialState)
+	store.dispatch(setMuseumId('-KhEMEsIQD90VeCmiaHA'))
+	// store.subscribe(saveState)
 
-  store.dispatch(setMuseumId('-KhEMEsIQD90VeCmiaHA'))
-  store.dispatch(fetchMuseum(store.getState().museum.id))
-
-  return store
+	return store
 }
 
 
 function uiSetup() {
-  // Needed for onTouchTap
-  // http://stackoverflow.com/a/34015469/988941
-  injectTapEventPlugin()
+	// Needed for onTouchTap
+	// http://stackoverflow.com/a/34015469/988941
+	injectTapEventPlugin()
 }
 
 function setupErrorHandler(store) {
-  const handleError = error => {store.dispatch(addError(error.message, error))}
-  // window.addEventListener('error', handleError)
-  // window.onerror = handleError
+	const handleError = error => {
+		store.dispatch(addError(error.message, error))
+	}
+	// window.addEventListener('error', handleError)
+	// window.onerror = handleError
 }
 
 function offlineSetup() {
-  // Install the ServiceWorker
-  OfflinePluginRuntime.install()
+	// Install the ServiceWorker
+	OfflinePluginRuntime.install()
 }
